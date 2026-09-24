e7b3bd7b80df4d98c3d4ff9209058263c6a54bd9
valoranttgm123-svg <valoranttgm123@gmail.com>
2026-09-24 12:37:58 +0700

    chore(ui): remove unused reference helper

--- components/AstraReferenceEntity.tsx
+++ components/AstraReferenceEntity.tsx
@@ -20,17 +20,6 @@ function mulberry32(seed: number) {
   };
 }
 
-function roundRectPath(ctx: CanvasRenderingContext2D, x: number, y: number, w: number, h: number, r: number) {
-  const rr = Math.min(r, w / 2, h / 2);
-  ctx.beginPath();
-  ctx.moveTo(x + rr, y);
-  ctx.arcTo(x + w, y, x + w, y + h, rr);
-  ctx.arcTo(x + w, y + h, x, y + h, rr);
-  ctx.arcTo(x, y + h, x, y, rr);
-  ctx.arcTo(x, y, x + w, y, rr);
-  ctx.closePath();
-}
-
 function faceHalfWidth(n: number) {
   const crown = Math.pow(Math.max(0, 1 - Math.pow((n + 0.02) / 1.02, 2)), 0.48);
   const jaw = 0.72 + 0.28 * (1 - Math.max(0, n));
